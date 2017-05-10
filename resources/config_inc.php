fef4407e26234cd5cd810384714f3c2a30023db6
AklerQ <akler.mortiferus@gmail.com>
2017-05-10 23:59:29 +0300

    Реализована поддержка ftp и telnet

---
+++ resources/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = 'root';
+$g_signup_use_captcha	  = OFF;
+
+
+$g_default_timezone       = 'Europe/Moscow';
+
+$g_crypto_master_salt     = 'y3tE9jtM+nzc164+IxQULbyFa389u5KMicVg3hwFYrI=';
