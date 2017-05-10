59f021c4ac75b45d00aad9b3767ba7a9c61429e2
AklerQ <akler.mortiferus@gmail.com>
2017-05-10 15:00:36 +0300

    Добавлен генератор строк и представление

--- fixture/navigation.py
+++ fixture/navigation.py
@@ -9,3 +9,8 @@ class NavigationHelper:
         if not ((len(wd.find_elements_by_link_text("Управление проектами")) > 0)
                 and (len(wd.find_elements_by_link_text("Оставаться в системе")) > 0)):
             wd.get(self.app.base_url)
+
+    def open_project_page(self):
+        wd = self.app.wd
+        if not wd.current_url.endswith('/manage_proj_page.php'):
+            wd.get('http://localhost/mantisbt-2.3.1/manage_proj_page.php')
